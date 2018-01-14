5089edbc05ca65740117561cd7a2ebd664fb4cea
mattperkins <github@mattperkins.com>
2018-01-14 17:46:16 +0000

    add UI vote selectors

--- src/index.js
+++ src/index.js
@@ -2,26 +2,61 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import myApp from './reducers'
-
-let store = createStore(myApp)
+import { voteLeave, voteRemain, voteUndecided } from './actions'
 
 class App extends React.Component {
-    // constructor(props) {
-    //     super(props)
-    // }
+    constructor(props) {
+        super(props)
+        this.store = this.props.store
+    }
+
+    handleVoteLeave = ()=>{
+        this.store.dispatch(voteLeave())
+    }
+
+    handleVoteRemain = () => {
+        this.store.dispatch(voteRemain())
+    }
+
+    handleVoteUndecided = () => {
+        this.store.dispatch(voteUndecided())
+    }
 
     render() {
         return (
-            <div className="masthead">
-                    <p>Hello</p>
+            <div className="masthead pull-up">
+            <div>
+                <h1>Second referendum?!</h1>
+                <h4>Select your choice below</h4>
+                <br />
+            <div>
+                        
+            <div className="vote">
+            <p onClick={this.handleVoteLeave}>Vote Leave</p>
             </div>
+            
+            <div className="vote">
+            <p onClick={this.handleVoteRemain}>Vote Remain</p>
+            </div>
+
+            <div className="vote">
+            <p onClick={this.handleVoteUndecided}>Vote Undecided</p>
+            </div>
+            
+            </div>
+            </div>
+            </div>
+
         )
     }
 }
 
+
+let store = createStore(myApp)
+
 function render()
 {   const render = document.getElementById('app')
-     ReactDOM.render(<App />, render)
+     ReactDOM.render(<App store={store} />, render)
 }
 store.subscribe(render)
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,14 +7,17 @@ const initialState = {
 export default (state = initialState, action) => {
     switch (action.type) {
         case 'VOTE_LEAVE':
+        console.log("Leave")
             return Object.assign({}, state, {
                 voteLeave: state.voteLeave + 1
             })
         case 'VOTE_REMAIN':
+        console.log("Remain")
             return Object.assign({}, state, {
-                VoteRemain: state.Remain + 1
+                voteRemain: state.voteRemain + 1
             })
         case 'VOTE_UNDECIDED':
+        console.log("Undecided")
             return Object.assign({}, state, {
                 voteUndecided: state.voteUndecided + 1
             })
